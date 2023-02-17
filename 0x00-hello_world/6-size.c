13bf1b472856f4c07f98f58af3fc075323023847
Mochiemo <Mochiemo Jose>
2023-02-17 03:12:30 -0800

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,12 @@ int main(void)
 	long c;
 	long long d;
 	float f;
-	
-printf("Size of a char: %l1 byte(s)\n", (unsigned long)sizeof(a));
-printf("size of an int: %l4 byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int: %l8 byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int: %l8 byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: %l4 byte(s)\n", (unsigned long)sizeof(f));
-return (0);
-}
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+        	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+		printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+		printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+		printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+		    return (0);
+}	
 
 
