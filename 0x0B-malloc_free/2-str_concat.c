3813a46f72fde2d277637c5832abc11a6f8d6af2
Mochiemo <Mochiemo Jose>
2023-03-13 14:37:31 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,36 +1,45 @@
-#include <main.h>
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-
 /**
- * str_concat - concatenate strings
- * @s1: string 1
- * @s2: string 2
- * Return: NULL if failure
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *s;
-	int a, b, c, d;
+	char *conct;
+	int i, ci;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == 0)
+	if (s2 == NULL)
 		s2 = "";
-	for (a = 0; s1[a] != '\0'; a++)
-		;
-	for (b = 0; s2[b] != '\0'; b++)
-		;
-	b++;
-	s = malloc(a * sizeof(*s1) + b * sizeof(*s2));
-	if (s == 0)
+		i = ci = 0;
+
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
+
+	if (conct == NULL)
 		return (NULL);
-	for (c = 0, d = 0; c < a + b; c++)
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
-		if (c < a)
-			s[c] = s1[c];
-		else
-			s[c] = s2[d++];
+		conct[i] = s1[i];
+		i++;
 	}
-	return (s);
+
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+
+		i++, ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
 }
+
